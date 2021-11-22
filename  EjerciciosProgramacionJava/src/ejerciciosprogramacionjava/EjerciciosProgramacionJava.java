90fa24e93ef4fdc28db6aecc4e92b9ee29af4212
a0y1959744c <a0y1959744c@S09-PC02>
2021-11-22 19:49:17 +0100

    Creado proyecto Java

---
+++  EjerciciosProgramacionJava/src/ejerciciosprogramacionjava/EjerciciosProgramacionJava.java
@@ -0,0 +1,21 @@
+/*
+ * Proyecto EjerciciosProgramacionJava - Archivo EjerciciosProgramacionJava.java - Compañía DAW 
+ * Licencia creative commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package ejerciciosprogramacionjava;
+
+/**
+ *
+ * @author Alex Pérez <perezalexisdaw@gmail.es>
+ */
+public class EjerciciosProgramacionJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
